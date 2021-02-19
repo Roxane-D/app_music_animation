13a1ca029a552a2c12f95cb2bcdf67a6178c9ff1
Roxane <r.dominguez@live.fr>
2021-02-19 16:45:06 +0100

    :sparkles: Responsive grid list & hero animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_grid/responsive_grid.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,28 +21,89 @@ class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          height: 300,
-          width: 300,
-          child: GestureDetector(
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => SecondPage()));
-            },
-            child: Hero(
-              tag: 'album-image',
-              child: Image.network(
-                  'https://www.wikilinks.fr/wp-content/uploads/2012/01/cover-pink-floyd-7.jpg'),
-            ),
+      body: GridSection(),
+    );
+  }
+}
+
+class GridSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ResponsiveGridList(
+        minSpacing: 10,
+        desiredItemWidth: 150,
+        children: [
+          AlbumCover(
+            albumTag: 'The Dark Side of th Moon',
+            albumUrl:
+                'https://www.wikilinks.fr/wp-content/uploads/2012/01/cover-pink-floyd-7.jpg',
           ),
+          AlbumCover(
+            albumTag: 'The Wall',
+            albumUrl:
+                'https://www.wikilinks.fr/wp-content/uploads/2012/01/cover-pink-floyd-10.jpg',
+          ),
+          AlbumCover(
+            albumTag: 'The Endless River',
+            albumUrl:
+                'https://www.wikilinks.fr/wp-content/uploads/2012/01/pinkfloydendlessriveralbumpochette2015.jpg',
+          ),
+          AlbumCover(
+            albumTag: 'The Final Cut',
+            albumUrl:
+                'https://www.wikilinks.fr/wp-content/uploads/2012/01/cover-pink-floyd-11.jpg',
+          ),
+          AlbumCover(
+            albumTag: 'The Division Bell',
+            albumUrl:
+                'https://www.wikilinks.fr/wp-content/uploads/2012/01/cover-pink-floyd-13.jpg',
+          ),
+          AlbumCover(
+            albumTag: 'Animals',
+            albumUrl:
+                'https://www.wikilinks.fr/wp-content/uploads/2012/01/cover-pink-floyd-9.jpg',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AlbumCover extends StatelessWidget {
+  final String albumUrl;
+  final String albumTag;
+
+  const AlbumCover({Key key, this.albumUrl, this.albumTag}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => AlbumPage(
+                        albumTag: albumTag,
+                        albumUrl: albumUrl,
+                      )));
+        },
+        child: Hero(
+          tag: albumTag,
+          child: Image.network(albumUrl),
         ),
       ),
     );
   }
 }
 
-class SecondPage extends StatelessWidget {
+class AlbumPage extends StatelessWidget {
+  final String albumUrl;
+  final String albumTag;
+
+  const AlbumPage({Key key, this.albumUrl, this.albumTag}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,9 +111,8 @@ class SecondPage extends StatelessWidget {
         backgroundColor: Colors.black,
       ),
       body: Hero(
-        tag: 'album-image',
-        child: Image.network(
-            'https://www.wikilinks.fr/wp-content/uploads/2012/01/cover-pink-floyd-7.jpg'),
+        tag: albumTag,
+        child: Image.network(albumUrl),
       ),
     );
   }
